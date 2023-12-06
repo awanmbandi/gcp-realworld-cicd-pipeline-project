1663ca46439aa32503d32a0fdfa22e599a26f5a2
Anyere Awankoudi Mbandi <68437700+awanmbandi@users.noreply.github.com>
2023-12-06 18:15:01 -0500

    Update configure-ansible-master-node.sh

--- archive/ansible/configure-ansible-master-node.sh
+++ archive/ansible/configure-ansible-master-node.sh
@@ -9,13 +9,14 @@ sudo sh -c 'echo ansible:ansibleadmin | chpasswd'
 sudo sed -i "s/PasswordAuthentication no/PasswordAuthentication yes/g" /etc/ssh/sshd_config
 sudo sed -i "s/.*#PermitRootLogin yes/PermitRootLogin yes/g" /etc/ssh/sshd_config
 sudo service sshd restart
-sudo echo "ansible ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+sudo su
+echo "ansible ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 
 ## Install Tomcat
-sudo yum install tomcat -y
-sudo systemctl enable tomcat
-sudo systemctl start tomcat
-sudo systemctl status tomcat
+yum install tomcat -y
+systemctl enable tomcat
+systemctl start tomcat
+systemctl status tomcat
 
 ## Wget
-sudo yum install wget -y
+yum install wget -y
